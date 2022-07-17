df69331a70040d44df63d195e2fe64723f678ef7
SujoyKrHaldar <sujoykrhaldar@gmail.com>
2022-07-17 22:58:11 +0530

    field update

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,13 +1,12 @@
 import createSchema from "part:@sanity/base/schema-creator";
 import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import blockContent from "./blockContent";
-import category from "./category";
-import post from "./post";
-import author from "./author";
+import blockContent from "./components/blockContent";
 import project from "./documents/project";
+import blog from "./documents/blog";
+import job from "./documents/job";
 
 export default createSchema({
   name: "default",
-  types: schemaTypes.concat([project, blockContent]),
+  types: schemaTypes.concat([project, blog, job, blockContent]),
 });
